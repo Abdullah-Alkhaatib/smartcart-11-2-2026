3226de39b43d17307f5906e6ed206baf1b633704
Devv-Abdullah <159600028+Devv-Abdullah@users.noreply.github.com>
2026-02-28 19:03:50 +0300

    وزز

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,50 @@
-const express = require('express');
-const connectToDB = require('./config/connectToDB.js');
-const cors = require('cors');
+const express = require("express");
+const connectToDB = require("./config/connectToDB.js");
+const cors = require("cors");
 const helmet = require("helmet"); // إضافة Helmet لتحسين الأمان
 const hpp = require("hpp");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 connectToDB();
 
-app.use(cors ({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL || "*",
-    credentials: true
-}));
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  }),
+);
 app.use(hpp());
 
-app.use('/images', express.static(__dirname + '/public/images')); // عشان نقدر نوصل للصور من خلال المسار /images
+app.use("/images", express.static(__dirname + "/public/images")); // عشان نقدر نوصل للصور من خلال المسار /images
 
 // Define routes
-const authRoute = require('./routes/authRoute.js');
-const userRoute = require('./routes/userRoute.js');
-const categoryRoute = require('./routes/categoryRoute.js');
-const productRoute = require('./routes/productRoute.js');
-const cartRoute = require('./routes/cartRoute.js');
-const contactUsRoute = require('./routes/contactUsRoute.js');
-const supportRoute = require('./routes/supportRoute.js');
-const orderRoute = require('./routes/orderRoute.js');
+const authRoute = require("./routes/authRoute.js");
+const userRoute = require("./routes/userRoute.js");
+const categoryRoute = require("./routes/categoryRoute.js");
+const productRoute = require("./routes/productRoute.js");
+const cartRoute = require("./routes/cartRoute.js");
+const contactUsRoute = require("./routes/contactUsRoute.js");
+const supportRoute = require("./routes/supportRoute.js");
+const orderRoute = require("./routes/orderRoute.js");
 
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/categories', categoryRoute);
-app.use('/api/products', productRoute);
-app.use('/api/cart', cartRoute);
-app.use('/api/contact', contactUsRoute);
-app.use('/api/support', supportRoute);
-app.use('/api/orders', orderRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/categories", categoryRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/contact", contactUsRoute);
+app.use("/api/support", supportRoute);
+app.use("/api/orders", orderRoute);
 
 app.get("/", (req, res) => {
   res.send("SmartCart API is running 🚀");
@@ -47,5 +53,5 @@ app.get("/", (req, res) => {
 // start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
